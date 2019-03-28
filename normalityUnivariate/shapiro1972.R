8621fd3e97b6d20971c88d25cd3c1424ddeea9a5
paulserafini <pserafin@sfu.ca>
2019-03-27 17:39:26 -0700

    Fix date on Shapiro-Francia

--- normalityUnivariate/shapiro1972.R
+++ normalityUnivariate/shapiro1972.R
@@ -20,7 +20,7 @@ shapiro1972 <- function (x) {
     z <- (log(1 - W) - mu)/sig
     pval <- pnorm(z, lower.tail = FALSE)
 
-    result <- list(method = "Shapiro and Francia (1973)",
+    result <- list(method = "Shapiro and Francia (1972)",
                    statistic = W,
                    df = NA,
                    p.value = pval)
