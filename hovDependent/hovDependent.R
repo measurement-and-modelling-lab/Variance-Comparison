62950309ad2353af479e74d896737715b007e4cc
paulserafini <pserafin@sfu.ca>
2019-04-16 11:29:30 -0700

    Fix Sandvik

--- hovDependent/hovDependent.R
+++ hovDependent/hovDependent.R
@@ -1,4 +1,4 @@
-hovDependent <- function(data,groups)
+hovDependent <- function(data, groups)
 {
     ## Test homogeneity of variances on several variables within a single group
 
@@ -7,7 +7,12 @@ hovDependent <- function(data,groups)
     source("./hovDependent/harris1985b.R")
     source("./hovDependent/harris1985c.R")
     source("./hovDependent/harris1985d.R")
-    dyn.load("./hovDependent/studentFit", local=FALSE)
+    source("./hovDependent/sandvik1982.R")
+    source("./hovDependent/functions/mvt.control.R")
+    source("./hovDependent/functions/commutation.R")
+    source("./hovDependent/functions/Student.R")
+    source("./hovDependent/functions/studentFit.R")
+    dyn.load("./hovDependent/functions/studentFit", local=FALSE)
 
     ## Prepare data
     sf <- studentFit(data)
@@ -18,10 +23,12 @@ hovDependent <- function(data,groups)
                     harris1985b(sf),
                     harris1985c(sf),
                     harris1985d(sf))
-    
-    if(length(unique(groups))){
-      output <- rbind(output,sandvick1982(data))
+
+    if (length(unique(groups))) {
+        output <- rbind(output,
+                        sandvik1982(data))
     }
+
     ## Format output
     output$statistic <- round(output$statistic, 2)
     output$p.value <- round(output$p.value, 2)
