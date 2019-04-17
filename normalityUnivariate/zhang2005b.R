d1839c4f29697cd6f10f8761b24d263ecc4da86e
paulserafini <pserafin@sfu.ca>
2019-04-16 21:43:49 -0700

    Fix Zhang-Wu doi

--- normalityUnivariate/zhang2005b.R
+++ normalityUnivariate/zhang2005b.R
@@ -6,7 +6,7 @@ zhang2005b <- function(x) {
     res <- sum((log((1 / phi - 1) / ((n - 0.5) / ((1:n) - 0.75) - 1))) ^ 2)
 
     result <- list(method = "Zhang-Wu (2005), Z<sub>C</sub>",
-                   doi = "https://doi.org/10.1093/biomet/64.2.415",
+                   doi = "https://doi.org/10.1016/j.csda.2004.05.034",
                    statistic = res,
                    df = NA,
                    p.value = NA)
